d41d6e90beec3e063aebeb88c203d951a322f920
Folexy13 <folajimiopeyemisax13@gmail.com>
2023-02-13 01:07:39 +0100

    ..

--- Backend/Model/clientModel.js
+++ Backend/Model/clientModel.js
@@ -24,6 +24,10 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    gender: {
+      type: String,
+      required: true,
+    },
     verified: {
       type: Boolean,
       default: false,
