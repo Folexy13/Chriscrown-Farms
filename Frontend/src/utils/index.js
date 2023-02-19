1589601be000a985f00f53f5751f8938cead318d
Folexy13 <folajimiopeyemisax13@gmail.com>
2023-02-20 00:01:36 +0100

    ..

--- Frontend/src/utils/index.js
+++ Frontend/src/utils/index.js
@@ -2,9 +2,10 @@ import SecureLS from "secure-ls";
 let ls = new SecureLS({ encodingType: "rc4", isCompression: true });
 
 export const getStoredAuthToken = () => ls.get("token");
-export const getClient = (token) => {
+export const getClient = () => {
   let user = atob(getStoredAuthToken().split(".")[1]);
-  return JSON.parse(user).user;
+  console.log(user);
+  return JSON.parse(user).user ? JSON.parse(user).user : JSON.parse(user);
 };
 export const storeAuthToken = (token) => ls.set("token", token);
 
